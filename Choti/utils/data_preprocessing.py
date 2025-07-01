b5517b44ddb4459394fa5e38d4b000b64cbe2f82
jgchoti <jgchotirat@gmail.com>
2025-07-01 10:10:06 +0200

    Add data preprocessing functions for feature encoding and scaling

---
+++ Choti/utils/data_preprocessing.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from typing import Tuple
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from utils.feature_engineering import add_lat_lon, add_cluster_loc, add_brussel_distances
+
+def encode_categorical_features(df, feature_list):
+    categorical_cols = ['province_encoded', 'type_encoded', 'subtype_encoded', 'postCode', 'location_cluster']
+    cols_to_encode = [col for col in categorical_cols if col in feature_list]
+    if cols_to_encode:
+        original_cols = set(df.columns)
+        df = pd.get_dummies(df, columns=cols_to_encode, drop_first=True)
+        new_cols = [col for col in df.columns if col not in original_cols]
+        feature_list = update_feature_list(feature_list, cols_to_encode, new_cols)
+    return df, feature_list
+    
+def data_preprocessing(df, model, feature_list, target, scale, location):
+    df = df.copy()
+
+    if any(col in feature_list for col in ["lat", "lon"]):
+        df = add_lat_lon(df)
+
+    if "location_cluster" in feature_list:
+        df = add_cluster_loc(df)
+
+    if "distance_from_brussels" in feature_list:
+        df = add_brussel_distances(df)
+        
+    if model in ["LinearRegression", "Ridge", "Lasso"]:
+        df, feature_list = encode_categorical_features(df, feature_list)
+    
+    X = df[feature_list]
+    y = df[target]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    if scale:
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)  
+        
+    return X_train, X_test, y_train, y_test
+
+
+def update_feature_list(feature_list, cols_to_encode, new_cols):
+    updated_features = [col for col in feature_list if col not in cols_to_encode]
+    updated_features.extend(new_cols)
+    
+    return updated_features
+
+
